c7a9234451622e6c776128dbea3261a401a99d0a
Onder Durak <105947075+Mantis322@users.noreply.github.com>
2022-09-17 22:06:58 +0300

    Update ToDoList.sol

--- ToDoList.sol
+++ ToDoList.sol
@@ -13,13 +13,16 @@ contract ToDo{
     // todos array'ini tanımlama / Declare todos array
     Todo[] public todos;
 
+    //Bu fonksiyon ile yaratılan structlar todos arrayine atılır / The structs that created with this function are thrown into the todos array.
     function create(string calldata _text) external {
+        // Girilen parametreleri array içine atar / Throw the entered parameters into the array
         todos.push(Todo({
             text : _text,
             completed : false
         }));
     }
-
+    
+    // Çağrılan index içindeki parametreleri güncelleyen fonksiyon / Function that updates parameters in the called index
     function update(uint _index,string calldata _text) external {
         todos[_index].text = _text;
         //Todo storage todo = todos[_index];
@@ -27,11 +30,13 @@ contract ToDo{
 
     }
 
+    //Çağrılan index içindeki parametreleri getirir(Ekstra gas'a mal olur) / Returns parameters in the called index(Costs extra gas)
     function get(uint _index) external view returns(string memory, bool){
         Todo storage todo = todos[_index];
         return (todo.text, todo.completed);
     }
-
+    
+    //Completed parametresini değiştirir / Changes the Completed parameter
     function toggleCompleted(uint _index) external {
         todos[_index].completed = !todos[_index].completed; 
     }
