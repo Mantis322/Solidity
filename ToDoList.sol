dddcec93b51c6ca5c6960ce31140b0707cf16bca
Onder Durak <105947075+Mantis322@users.noreply.github.com>
2022-09-17 22:00:41 +0300

    Update ToDoList.sol

--- ToDoList.sol
+++ ToDoList.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.17;
 // SPDX-License-Identifier: MIT
 
 contract ToDo{
+    
+    // Struct için ihtiyacımız olan değişkenleri tanımlama / Declare the variables we need for the struct
     struct Todo{
         string text;
         bool completed;
     }
 
+    // todos array'ini tanımlama / Declare todos array
     Todo[] public todos;
 
     function create(string calldata _text) external {
